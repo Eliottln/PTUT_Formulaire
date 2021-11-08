49e560bce9df4470dfed1ec1660da63ff9d06b85
LE NORMAND ELIOTT p2006406 <eliott.le-normand@etu.univ-lyon1.fr>
2021-11-08 10:12:23 +0100

    ajout page accueil

---
+++ creation.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+
+    <meta charset="utf-8">
+    <title>Accueil</title>
+    <link type="text/css" rel="stylesheet" href="">
+    <link rel="icon" type="image/png" sizes="16x16" href="">
+
+</head>
+
+    <body>
+
+        <button type="button" id="new-element">Nouveau</button>
+
+        <form id="form-document" action="https://ressources.site/" method="post">
+
+            <button id="submit" type="submit" disabled>Enregistrer</button>
+        </form>
+
+        <script src="/js/newForm.js"></script>
+
+    </body>
+
+
+</html>
--- index.php
+++ index.php
@@ -1,26 +1,63 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
 
-    <meta charset="utf-8">
-    <title>Accueil</title>
-    <link type="text/css" rel="stylesheet" href="">
-    <link rel="icon" type="image/png" sizes="16x16" href="">
+    <head>
 
-</head>
+        <meta charset="utf-8">
+        <title>Accueil</title>
+        <link type="text/css" rel="stylesheet" href="">
+        <link rel="icon" type="image/png" sizes="16x16" href="">
+
+    </head>
 
     <body>
 
-        <button type="button" id="new-element">Nouveau</button>
+        <header>
 
-        <form id="form-document" action="https://ressources.site/" method="post">
+        </header>
 
-            <button id="submit" type="submit" disabled>Enregistrer</button>
-        </form>
 
-        <script src="/js/newForm.js"></script>
+        <main>
+
+            <form id="form-sign-in" action="https://ressources.site/" method="post">
+
+                <input id="fname1" type="text" name="fname1">
+                <label for="fname1">Prénom</label>
+
+                <input id="lname1" type="text" name="lname1">
+                <label for="lname1">Nom</label>
+
+                <input id="email1" type="email" name="email1">
+                <label for="email1">Email</label>
+
+                <input id="password1" type="password" name="password1">
+                <label for="password1">Mot de passe</label>
+
+                <input id="password-repeat" type="password" name="password-repeat">
+                <label for="password-repeat">Répéter mot de passe</label>
+
+                <button id="b-sign-in" type="submit">S'inscrire</button>
+
+            </form>
+
+            <form id="form-log-in" action="https://ressources.site/" method="post">
+
+                <input id="email2" type="email" name="email2">
+                <label for="email2">Email</label>
+
+                <input id="password2" type="password" name="password2">
+                <label for="password2">Mot de passe</label>
+
+                <button id="b-log-in" type="submit">Se connecter</button>
+
+            </form>
+
+        </main>
+
+        <footer>
+
+        </footer>
 
     </body>
-
 
 </html>
