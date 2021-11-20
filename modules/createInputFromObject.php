91ced4a3280bd7781dbd8e7134a8351c2b7a791e
FORESTIER ALEXIS p2004100 <alexis.forestier2@etu.univ-lyon1.fr>
2021-11-20 01:15:24 +0100

    HTML niv3 + fusion "creation/CreateForm"

---
+++ modules/createInputFromObject.php
@@ -0,0 +1,44 @@
+
+<?php
+// Ce module doit être placé dans la balise "script" et après avoir inclue "ImportFile.php"
+include_once($_SERVER["DOCUMENT_ROOT"]."/class/input.php");
+
+if(!empty($arrayObjectInput)){
+    echo "let arrayValues;\n";
+    for($i=0;$i<count($arrayObjectInput);$i++){
+        switch($arrayObjectInput[$i]->getType()){
+            
+            case "select":
+                $arrayToString = "[";
+                foreach($arrayObjectInput[$i]->getSubInput() as $key => $value){
+                    $arrayToString .= "\"".$value->getValue()."\"";
+                    if($key+1 < count($arrayObjectInput[$i]->getSubInput())){
+                        $arrayToString .= ",";
+                    }
+                }
+                $arrayToString .= "];";
+                echo "arrayValues = ".$arrayToString;
+                echo "\naddSelectFromObject(\"".$arrayObjectInput[$i]->getName()."\",arrayValues);\n";
+                break;
+
+            case "checkbox":
+            case "radio":
+                $arrayToString = "[";
+                foreach($arrayObjectInput[$i]->getValue() as $key => $value){
+                    $arrayToString .= "\"".$value."\"";
+                    if($key+1 < count($arrayObjectInput[$i]->getValue())){
+                        $arrayToString .= ",";
+                    }
+                }
+                $arrayToString .= "];";
+                echo "arrayValues = ".$arrayToString;
+                echo "\naddRadioOrCheckboxFromObject(\"".$arrayObjectInput[$i]->getName()."\",\"".$arrayObjectInput[$i]->getType()."\",arrayValues);\n";
+                break;
+
+            default:
+                echo "addQuestionFromObject(\"".$arrayObjectInput[$i]->getName()."\",\"".$arrayObjectInput[$i]->getType()."\");\n";
+        }
+        
+    }
+}
+?>
