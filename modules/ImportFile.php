e84c623d93ebf163a569ce592f2d872af4221046
FORESTIER ALEXIS p2004100 <alexis.forestier2@etu.univ-lyon1.fr>
2021-11-22 18:43:32 +0100

    compatibility php 8 and inferior

--- modules/ImportFile.php
+++ modules/ImportFile.php
@@ -1,6 +1,14 @@
 <?php
 include_once($_SERVER["DOCUMENT_ROOT"]."/class/input.php");
 
+function contains($a, $b){
+    try {
+        return str_contains($a, $b);
+    } catch (\Throwable $th) {
+        return strpos($a, $b) !== false;
+    }
+}
+
 /**
  * @return returnArray
  * returnArray[0] = "type"
@@ -9,10 +17,10 @@ include_once($_SERVER["DOCUMENT_ROOT"]."/class/input.php");
 function getNameAndType($array){
     $returnArray = array();
     for($i = 0; $i < count($array); $i++){
-        if(str_contains($array[$i], "name=")){
+        if(contains($array[$i], "name=")){
             array_push($returnArray,$array[$i+1]);
         }
-        if(str_contains($array[$i], "type=")){
+        if(contains($array[$i], "type=")){
             array_push($returnArray,$array[$i+1]);
         }
     }
@@ -21,7 +29,7 @@ function getNameAndType($array){
 
 function getValue($array){
     for($i = 0; $i < count($array);$i++){
-        if(str_contains($array[$i],"value")){
+        if(contains($array[$i],"value")){
             return $array[$i+1];
         }
     }
@@ -59,7 +67,7 @@ if(!empty($_POST)){
                 $arrayStringInput = array();   //Data
                 $currentSelect;
                 foreach($arrayStringForm as $key => $value){
-                    if(str_contains($value, "input ")){
+                    if(contains($value, "input ")){
                         array_push($arrayStringInput,explode("\"",$value));
                         $typeAndName = getNameAndType(explode("\"",$value));
                         
@@ -72,17 +80,17 @@ if(!empty($_POST)){
                         }
                     
                     }
-                    else if(str_contains($value, "select ")){
+                    else if(contains($value, "select ")){
                         array_push($arrayStringInput,explode("\"",$value));
                         $typeAndName = getNameAndType(explode("\"",$value));
                         $currentSelect = new Input("select",$typeAndName[0]);
                     }
-                    else if(str_contains($value, "option ")){
+                    else if(contains($value, "option ")){
                         array_push($arrayStringInput,explode("\"",$value));
                         $valueExtracted = getValue(explode("\"",$value));
                         $currentSelect->addOption($valueExtracted);
                     }
-                    else if(str_contains($value, "/select>")){
+                    else if(contains($value, "/select>")){
                         array_push($arrayStringInput,explode("\"",$value));
                         array_push($arrayObjectInput, $currentSelect);
                         $currentSelect = null;
@@ -92,7 +100,8 @@ if(!empty($_POST)){
                 echo "Object : ";
                 var_dump($arrayObjectInput);
                 echo "Fichier HTML importé avec succés";
-            } catch (\Throwable $th) {
+            } catch (Exception $e) {
+                echo $e->getLine().":".$e->getMessage()."<br>";
                 echo "L'importation du fichier HTML a échouée";
             }
             
