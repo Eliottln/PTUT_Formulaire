761ce3578e11c23902e28e1edecc0023e3152ee2
FORESTIER ALEXIS p2004100 <alexis.forestier2@etu.univ-lyon1.fr>
2021-12-08 00:22:12 +0100

    add title and more

--- include/includeDATABASE.php
+++ include/includeDATABASE.php
@@ -2,11 +2,10 @@
 
 // Create connection
 try {
-    //code...
 
-    $dsn = 'mysql:dbname=ptut_site_gestion_form; host=127.0.0.1';  // 'mysql:dbname=p2008444; host=iutbg-lamp.univ-lyon1.fr';
-    $username = "Hedi"; //"Hedi";
-    $password = "hedizair120"; //"hedizair120";
+    $dsn = 'mysql:dbname=p2008444; host=iutbg-lamp.univ-lyon1.fr';
+    $username = "p2008444";
+    $password = "12008444";
 
     $connect = new PDO($dsn, $username, $password);
     $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <title>Accueil</title>
     <link type="text/css" rel="stylesheet" href="css/style.css">
-    <link rel="icon" type="image/png" sizes="16x16" href="">
+    <link rel="icon" type="image/ico" href="/img/favicon.ico" />
 
 </head>
 
@@ -53,6 +53,16 @@
 
         </form>
 
+        <div class="slider">&laquo;</div>
+
+        <div id="brand" class="noselect">
+            <span id="UwU">U<span>w</span>U</span>
+            <div>ni</div>
+            <div>form</div>
+        </div>
+
+        <div class="slider">&raquo;</div>
+
         <form id="form-login" action="/login.php" method="post">
 
             <h2>Se connecter</h2>
@@ -80,6 +90,11 @@
 </body>
 
 <script>
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+
     let inputs = document.querySelectorAll('#LogInSignUp main form > div input');
 
     function inputNotEmpty() {
@@ -95,6 +110,39 @@
     for (let index = 0; index < inputs.length; index++) {
         inputs[index].addEventListener('keyup', inputNotEmpty);
     }
+
+    const logo = document.getElementById('UwU');
+    let state = 0
+
+    function expression() {
+        let value = getRandomInt(5)
+        if(value==state){
+            value = (state + 1)%5;
+        }
+        switch (value) {
+            case 0:
+                logo.innerHTML = 'U<span>w</span>U'
+                break;
+
+            case 1:
+                logo.innerHTML = 'X<span>o</span>X'
+                break;
+
+            case 2:
+                logo.innerHTML = '^<span>w</span>^'
+                break;
+
+            case 3:
+                logo.innerHTML = '><span>_</span><'
+                break;
+
+            case 4:
+                logo.innerHTML = 'O<span>w</span>O'
+                break;
+        }
+    }
+
+    logo.addEventListener('click', expression);
 </script>
 
 
