8fd56b4c2912197e162738bbfc133ffcca332282
Hedi Zair <hedi.marwan@hotmail.fr>
2021-12-08 21:23:41 +0100

    visuForm done

--- exportBdd.php
+++ exportBdd.php
@@ -2,7 +2,7 @@
 
 
 
-
+$nQuestion = 0;
 $nbChamps = count($_POST);
 
 if($nbChamps == 0){
@@ -17,8 +17,11 @@ $tabOfMultipleAnswerQuestion = array(); //Contiens toutes les questions à choix
 try{
     //$connect = new PDO($dsn,$username,$password);
     $connect = new PDO("sqlite:../database.db");
+
     $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "INSERT INTO Document VALUES (null,". $nbChamps.");"; //Document
+
+    //On crée un document, on ne connait pas encore le nb de questions donc on le met à 0 pour le modifier plus bas
+    $sql = "INSERT INTO Document VALUES (null,0);"; //Document
     $connect->exec($sql);
     $lastID = $connect->lastInsertId(); //Numéro du document
     $sql = '';
@@ -53,7 +56,7 @@ try{
 
             default: // Le cas des champ de question seul, ou des champs de question d'en tete de choix de radio ou check --> (text area)
                 $sql .= "INSERT INTO Form VALUES('" . $numQuestion . "','" . $lastID . "','" . $typeOfInput . "','" . $valueOfInput . "');";
-
+                $nQuestion++;
                 break;
 
         }
@@ -61,9 +64,15 @@ try{
 
     }
 
+
+
     $connect->exec($sql);
     //$prepQuery = $connect->prepare($sql);
     //$prepQuery->execute();
+
+    //On ajoute le nombre de question dans le champ nbQuestion dans la table Document
+    $sql = 'UPDATE Document SET nbQuestions='. $nQuestion .' WHERE idDocument= '.$lastID .' ;';
+    $connect->exec($sql);
 
 
 
--- visuForm.php
+++ visuForm.php
@@ -15,6 +15,14 @@
         <?php require 'modules/header.php'; ?>
 
         <main>
+
+            <div>
+                <h1><?php echo 'Formulaire numéro : '. $_GET['identity'] ?></h1>
+            </div>
+
+            <form action="">
+
+
             <?php
 
 
@@ -25,10 +33,45 @@
 
                 $sql = $connect->query("SELECT * FROM Form WHERE idDocument = ". $_GET['identity'] ."  ")->fetchAll();
 
-                echo "Nombre de questions sur ce formulaire : " . count($sql) . "<br>";
+
 
                 foreach ($sql as $key => $value){
-                    echo $value['typeOfQuestion'] . "<br>";
+
+
+                    switch ($value['typeOfQuestion']){
+                        case 'question':
+                            echo addQuestion($value['descript'],$value['idForm']);
+                            break;
+                        case 'radioQuestion':
+
+                            $sql2 = $connect->query('SELECT * FROM RadioChoice WHERE idForm='.$value['idForm'].' AND idDocument='.$value['idDocument'].';')->fetchAll();
+                            $tabRadioLab = array();
+                            foreach ($sql2 as $key2 => $value2){
+                                array_push($tabRadioLab,$value2['descript']);
+                            }
+
+                            echo addRadioQuestion($value['descript'],$value['idForm'],$tabRadioLab);
+                            break;
+
+                        case 'checkBoxQuestion':
+                            $sql3 = $connect->query('SELECT * FROM CheckBoxChoice WHERE idForm='.$value['idForm'].' AND idDocument='.$value['idDocument'].';')->fetchAll();
+                            $tabRadioLab = array();
+                            foreach ($sql3 as $key3 => $value3){
+                                array_push($tabRadioLab,$value3['descript']);
+                            }
+
+                            echo addCheckBoxQuestion($value['descript'],$value['idForm'],$tabRadioLab);
+                            break;
+
+                        case 'date':
+                            echo addDate($value['descript'],$value['idForm']);
+                            break;
+
+                        default:
+
+                    }
+
+
                 }
 
 
@@ -43,7 +86,11 @@
 
 
             ?>
-
+                <div >
+                    <button id="S" type="button" >Envoyer</button>
+                    <button id="R" type="reset">Effacer</button>
+                </div>
+            </form>
 
         </main>
 
@@ -53,3 +100,58 @@
 
 
 </html>
+
+<?php
+
+function addQuestion($_descript, $_id){
+    return '<div id="question-'. $_id .'-text">
+                <label for="question-'.$_id .'" > '. $_descript.'</label>
+                <input id="question-'. $_id  .'" type="text" name="question-'. $_id .'" required>
+            </div>
+    ';
+}
+
+function addDate($_descript, $_id){
+    return '<div id="question-'. $_id .'-date">
+                <label for="question-'.$_id .'" > '. $_descript.'</label>
+                <input id="question-'. $_id  .'" type="date" name="question-'. $_id .'" required>
+            </div>
+    ';
+}
+
+function addRadioQuestion($_descript, $_idQuestion, array $_tabRadioLabel):string{
+    $resultat =  '<div id="question-'. $_idQuestion .'-radio">
+                        <label> ' . $_descript . '</label>
+                        <div>';
+
+    for($i = 0; $i < count($_tabRadioLabel); $i++){
+        $resultat .= '<div> 
+                            <input class="radio" name="question-'. $_idQuestion .'" value="'. $_tabRadioLabel[$i] .'" type="radio" id="question-'. $_idQuestion .'-'. $i.'" >
+                            <label for="question-'. $_idQuestion .'-'. $i.'"> '. $_tabRadioLabel[$i] .'</label>
+                      </div>   ';
+    }
+
+    $resultat .= '    </div>
+                </div>';
+
+    return $resultat;
+}
+
+
+function addCheckBoxQuestion($_descript, $_idQuestion, array $_tabCheckBoxLabel):string{
+    $resultat =  '<div id="question-'. $_idQuestion .'-checkbox">
+                        <label> ' . $_descript . '</label>
+                        <div>';
+
+    for($i = 0; $i < count($_tabCheckBoxLabel); $i++){
+        $resultat .= '<div> 
+                            <input class="checkbox" name="question-'. $_idQuestion .'[]" value="'. $_tabCheckBoxLabel[$i] .'" type="checkbox" id="question-'. $_idQuestion .'-'. $i.'" >
+                            <label for="question-'. $_idQuestion .'-'. $i.'"> '. $_tabCheckBoxLabel[$i] .'</label>
+                      </div>   ';
+    }
+
+    $resultat .= '    </div>
+                </div>';
+
+    return $resultat;
+}
