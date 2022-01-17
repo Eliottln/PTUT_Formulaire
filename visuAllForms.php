f7bbf091be340376229855b8366bcf8ada7d8def
FORESTIER ALEXIS p2004100 <alexis.forestier2@etu.univ-lyon1.fr>
2022-01-17 12:22:31 +0100

    CSS visuAllForms + search

--- visuAllForms.php
+++ visuAllForms.php
@@ -2,39 +2,47 @@
 include_once($_SERVER["DOCUMENT_ROOT"] . "/include/config.php");
 include_once($_SERVER["DOCUMENT_ROOT"] . "/include/includeDATABASE.php");
 
-if(empty($_SESSION['user']) || empty($_SESSION['user']['id'])){
+if (empty($_SESSION['user']) || empty($_SESSION['user']['id'])) {
     header('Location: index.php');
     exit();
 }
 
-function displayAllForm($connect){
+function displayAllForm($connect)
+{
     $forms = "";
     try {
 
         $date = $connect->quote(date("Y-m-d"));
-        $sql = $connect->query("SELECT * FROM Forms WHERE expire >= ".$date." OR expire = ''")->fetchAll();
+        if(!empty($_GET['search'])){
+            $sql = $connect->query("SELECT * FROM Forms WHERE (expire >= " . $date . " OR expire = '') AND LOWER(title) LIKE '%".strtolower($_GET['search'])."%'")->fetchAll();
+        }
+        else{
+            $sql = $connect->query("SELECT * FROM Forms WHERE expire >= " . $date . " OR expire = ''")->fetchAll();
+        }
         
 
-        foreach ($sql as $value){
 
-            $forms .=   '<div class="blocArticle">   
-                            <p> IdDoc = '. $value['id'].'</p>
-                            <a href="visuForm.php?identity='.$value['id'].'">
-                                <img style="width: 50px; height: 50px" src="img/formulaire.png" alt="Prévisualisation">
-                            </a> 
-                            <p> Titre : '. $value['title'] .'</p>
-                            <p> Nb Question : '. $value['nb_question'] .'</p>
+        foreach ($sql as $value) {
+
+            $forms .=   '<div class="blocArticle">
+                            <div>
+                                <a href="#">
+                                    <p>ID #' . $value['id'] . '</p>
+                                    <div>
+                                        <img src="img/formulaire.png" alt="image form">
+                                    </div>
+                                    <p>Titre : ' . $value['title'] . '</p>
+                                    <p>' . $value['nb_question'] . ' question' . (($value['nb_question'] > 1) ? "s" : null) . '</p>
+                                </a>
+                            </div>
                         </div>';
         }
-
     } catch (PDOException $e) {
-        if(!empty($_SESSION['user']) && $_SESSION['user']['admin'] == 1){
-            echo 'Erreur sql : (line : '. $e->getLine() . ") " . $e->getMessage();
-        }
-        else if(!empty($_SESSION['user']) && $_SESSION['user']['admin'] == 0){
+        if (!empty($_SESSION['user']) && $_SESSION['user']['admin'] == 1) {
+            echo 'Erreur sql : (line : ' . $e->getLine() . ") " . $e->getMessage();
+        } else if (!empty($_SESSION['user']) && $_SESSION['user']['admin'] == 0) {
             echo 'Il semblerait que les formulaires ne sont pas accessible';
         }
-        
     }
     return $forms;
 }
@@ -49,30 +57,38 @@ $pageName = "Tous les Forms";
 include_once($_SERVER["DOCUMENT_ROOT"] . "/modules/head.php");
 ?>
 
-    <body>
+<body>
 
-        <?php require 'modules/header.php'; ?>
+    <?php require 'modules/header.php'; ?>
 
-        <main>
+    <main id="visuAll">
 
-            <?= displayAllForm($connect)?>
+        <div>
+            <form class="search" action="/visuAllForms.php" method="get">
+                <input type="search" name="search">
+                <button type="submit"><span class="gg-search"></span></button>
+            </form>
+        </div>
 
+        <div>
+            <?= displayAllForm($connect) ?>
+        </div>
 
-        </main>
+    </main>
 
-        <?php require 'modules/footer.php'; ?>
+    <?php require 'modules/footer.php'; ?>
 
-    </body>
+</body>
 
-    <script>
-        <?php
-        if (isset($_SESSION['formNotFound'])) {
-            unset($_SESSION['formNotFound']);
-            echo 'alert("le form '.$_SESSION['formNotFoundID'].' n\'est plus accessible")';
-            unset($_SESSION['formNotFoundID']);
-        }
-        ?>
-    </script>
+<script>
+    <?php
+    if (isset($_SESSION['formNotFound'])) {
+        unset($_SESSION['formNotFound']);
+        echo 'alert("le form ' . $_SESSION['formNotFoundID'] . ' n\'est plus accessible")';
+        unset($_SESSION['formNotFoundID']);
+    }
+    ?>
+</script>
 
 
 </html>
